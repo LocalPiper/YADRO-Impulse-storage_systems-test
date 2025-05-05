990d03d411f97f3c0f91617f7ad0dbba30bcf0a4
Local Piper <artsor53@gmail.com>
2025-05-05 04:23:54 +0300

    chore: queue and tables

--- club.cpp
+++ club.cpp
@@ -1,6 +1,22 @@
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+struct WaitingQueue {
+  std::queue<std::string> q;
+};
+
+struct Tables {
+  std::unordered_map<int, std::pair<std::string, int>> tableToClient;
+  std::unordered_map<std::string, int> clientToTable;
+};
+
 class Club {
-  int number_of_tables;
+  int numberOfTables;
   int costPerHour;
   int startTime;
   int endTime;
+  WaitingQueue q;
+  Tables t;
 };
