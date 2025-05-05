dd05618271773b4bbffc3f573566f7fe1462ee8f
Local Piper <artsor53@gmail.com>
2025-05-05 05:10:38 +0300

    chore: records

--- club.cpp
+++ club.cpp
@@ -13,6 +13,11 @@ struct Tables {
   std::unordered_map<std::string, int> clientToTable;
 };
 
+struct Record {
+  long long accumulatedCost;
+  int timeOperating;
+};
+
 class Club {
   int numberOfTables;
   int costPerHour;
@@ -20,11 +25,16 @@ class Club {
   int endTime;
   WaitingQueue waitingQueue;
   Tables tables;
+  std::unordered_map<int, Record> tableRecords;
 
 public:
   Club(int num, int cost, std::string start, std::string end)
       : numberOfTables(num), costPerHour(cost), startTime(timeToInt(start)),
         endTime(timeToInt(end)), waitingQueue({std::queue<std::string>()}),
         tables({std::unordered_map<int, std::pair<std::string, int>>(),
-                std::unordered_map<std::string, int>()}) {}
+                std::unordered_map<std::string, int>()}) {
+    for (int x = 1; x <= num; ++x) {
+      tableRecords[x] = {0, 0};
+    }
+  }
 };
